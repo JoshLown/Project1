246c97aa3ea79ea0c3e6f4573de6ebabf3612586
tennzzin <135814908+tennzzin@users.noreply.github.com>
2023-09-19 12:13:44 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,46 @@
 import java.awt.Color;
 public class Rectangle {
+    public Rectangle(double x, double y, double w, double h) {
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
+    private double xPos;
+    private double yPos;
+    private double width;
+    private double height;
+    public double calculatePerimeter() {
+        return 2*width + 2*height;
+    }
+    public double calculateArea() {
+        return width * height;
+    }
+    public void setColor(Color color) {
+    }
+    public void setPos(double x, double y) {
+        xPos = x;
+        yPos = y;
+    }
+    public void setHeight(double h) {
+        height = h;
+    }
+    public void setWidth(double w) {
+        width = w;
+    }
+    public Color getColor(Color color) {
+        return color;
+    }
+    public double getXPos(double x) {
+        return xPos;
+    }
+    public double getYPos(double y) {
+        return yPos;
+    }
+    public double getWidth(double w) {
+        return width;
+    }
+    public double getHeight(double h) {
+        return height;
+    }
 }
