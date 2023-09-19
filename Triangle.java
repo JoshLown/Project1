db83e4f8a06c41db3a77b80b14b4b3ca22813eb2
tennzzin <135814908+tennzzin@users.noreply.github.com>
2023-09-19 12:14:02 -0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -11,6 +11,35 @@ public class Triangle {
     private double width;
     private double height;
     public double calculatePerimeter() {
-
+    }
+    public double calculateArea() {
+        return (width*height)/2;
+    }
+    public void setColor(Color color) {
+    }
+    public void setPos(double x, double y) {
+        xCoor = x;
+        yCoor = y;
+    }
+    public void setHeight(double h) {
+        height = h;
+    }
+    public void setWidth(double w) {
+        width = w;
+    }
+    public Color getColor(Color color) {
+        return color;
+    }
+    public double getXPos(double x) {
+        return xCoor;
+    }
+    public double getYPos(double y) {
+        return yCoor;
+    }
+    public double getHeight(double h) {
+        return height;
+    }
+    public double getWidth(double w) {
+        return width;
     }
 }
